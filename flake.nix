7316a62d5b6bc7437551b5e20d65d3c13f7fd2e0
cullvox <cullvox@outlook.com>
2022-12-17 22:21:30 -0500

    First Commit

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "Cullvox's NixOS config";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    utils.url = "github:gytis-ivaskevicius/flake-utils-plus";
+    nixos-hardware.url = "github:nixos/nixos-hardware/master";
+    # nix-gaming.url = "github:fufexan/nix-gaming";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = inputs @ { self, nixpkgs, utils, home-manager, ... }:
+    let
+      user = "cullvox";
+      # localOverlays = import ./packages/overlays {inherit inputs;};
+      # inputOverlays = _: super: {
+      # };
+
+    in utils.lib.mkFlake {
+      inherit self inputs user;
+
+      channelsConfig.allowUnfree = true;
+      # sharedOverlays = [ inputOverlays localOverlays ];
+
+      hostDefaults.extraArgs = { inherit inputs user; };
+      hostDefaults.modules = [
+        home-manager.nixosModules.home-manager {
+          # home-manager.useGlobalPkgs = true; # Probably not needed
+          home-manager.useUserPackages = true;
+          home-manager.extraSpecialArgs = { inherit user; };
+        }
+      ];
+
+      hosts = {
+        nix-desktop.modules = [
+          ./configuration.nix
+          inputs.nixos-hardware.nixosModules.common-cpu-intel-kaby-lake
+          inputs.nixos-hardware.nixosModules.common-gpu-nvidia-nonprime
+          home-manager.nixosModules.home-manager {
+            home-manager.users.${user}.imports = [
+ #             ./home.nix
+            ];
+          }
+        ];
+      };
+  };
+}
